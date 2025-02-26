e925a12faee09d2e34ec025c315c693f99c5fcb7
Italo Falasca <italorox123@hotmail.com>
2025-02-25 21:21:00 -0300

    Att 25.02.25
    Att Registro Civil Site Cartorio

--- html-css/projetos/projeto-site-cartorio/script.js
+++ html-css/projetos/projeto-site-cartorio/script.js
@@ -1,7 +1,32 @@
-    // Função para desmarcar o checkbox, fechando o menu
+// Função para desmarcar o checkbox, fechando o menu
     function fecharMenu() {
         document.getElementById("menu-toggle").checked = false;
     }
     function fecharMenuMobile() {
         document.getElementById("menu-toggle").checked = false;
     }
+
+// JAVASCRIPT do REGISTRO CIVIL 
+
+function toggleItem(element) {
+    let item = element.parentElement;
+    let conteudo = item.querySelector('.conteudo');
+
+    if (item.classList.contains('ativo')) {
+        conteudo.style.maxHeight = null;
+        item.classList.remove('ativo');
+    } else {
+        // Fecha os outros itens abertos
+        document.querySelectorAll('.item.ativo').forEach(outroItem => {
+            outroItem.classList.remove('ativo');
+            outroItem.querySelector('.conteudo').style.maxHeight = null;
+        });
+
+        // Expande o item clicado ajustando a altura real do conteúdo
+        conteudo.style.maxHeight = conteudo.scrollHeight + "px";
+        item.classList.add('ativo');
+    }
+}
+
+
+// 
