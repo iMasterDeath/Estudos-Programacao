2afc705dc59c983a93eb1a210d44b7de5cb2d7e3
Italo Falasca <98832778+iMasterDeath@users.noreply.github.com>
2025-04-25 09:33:55 -0300

    Att Reposição, Backup
    - Corrigido tela Contato e atualização de repositório e backup

---
+++ html-css/projetos/projeto-site-cartorio backup antigo/script.js
@@ -0,0 +1,7 @@
+    // Função para desmarcar o checkbox, fechando o menu
+    function fecharMenu() {
+        document.getElementById("menu-toggle").checked = false;
+    }
+    function fecharMenuMobile() {
+        document.getElementById("menu-toggle").checked = false;
+    }
